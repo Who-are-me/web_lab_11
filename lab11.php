de1a5ffdecf3014ab69a517da433b82a651d8580
Who-are-me <vasyastorchak74@gmail.com>
2021-03-18 15:58:32 +0200

    lab work 11 v2

--- lab11.php
+++ lab11.php
@@ -16,5 +16,11 @@ echo "riz= $riz<br>";
 echo "dob= $dob<br>";
 echo "dil= $dil<br>";
 
- 
+$e = 2.718281;
+
+$q = ( abs($_POST["d"] + $_POST["f"]) + tan(pow($_POST["f"], 2) + pow($_POST["h"], 2)) ) / ($_POST["k"] + 4 * log($_POST["f"]));
+$u = ($_POST["h"] + pow(sin($_POST["k"]), 2) + cos($_POST["h"])) / (pow($e, $_POST["f"]) + $_POST["d"] * log($_POST["h"] + $_POST["k"]));
+
+echo "<br>q= $q<br>";
+echo "u= $u<br>";
 ?>
